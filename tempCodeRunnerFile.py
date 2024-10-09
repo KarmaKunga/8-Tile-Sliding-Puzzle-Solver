dca6db2084917c2237dbd09df8d14a7d447a191d
KarmaKunga <karmakungab@gmail.com>
2024-10-09 19:50:39 +0700

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-    #     new_loc = location(loc.i, loc.j-1)
-    #     new_tiles = tiles
-    #     new_tiles[loc.i][loc.j], new_tiles[new_loc.i][new_loc.j] = new_tiles[new_loc.i][new_loc.j], new_tiles[loc.i][loc.j]
-    #     moves.append([new_tiles, new_loc])
