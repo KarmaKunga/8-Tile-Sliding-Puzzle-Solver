6b8d9c9d8376dc856cf08a211db57c29fb4a1f55
KarmaKunga <karmakungab@gmail.com>
2024-10-09 19:50:15 +0700

    Added location, possible moves, heuristic
    - Made location into its own class
    - Added a function that generates possible moves
    - Added function to compute heuristic

--- 8TSP-Solver.py
+++ 8TSP-Solver.py
@@ -1,7 +1,17 @@
 # Author: Kunga
 # Created: 09/10/2024
 
+from copy import deepcopy
+
 N = 3  # Tiles / Side
+
+class location:
+    def __init__(self, i, j):
+        self.i = int(i)
+        self.j = int(j)
+    
+    def __str__(self):
+        return f"{self.i}, {self.j}"
 
 def printTile(tiles):
     def printLine():
@@ -58,15 +68,66 @@ def check_X(tiles):
     for y in range(0, N):
         for x in range(0, N):
             if tiles[y][x] == "X":
-                return y, x                
+                return location(y, x)                
     return None
+
+def possible_moves(tiles, loc):
+    moves = []
+
+    if (loc.i > 0):
+        new_loc = location(loc.i-1, loc.j)
+        new_tiles = deepcopy(tiles)
+        new_tiles[loc.i][loc.j], new_tiles[new_loc.i][new_loc.j] = new_tiles[new_loc.i][new_loc.j], new_tiles[loc.i][loc.j]
+        moves.append([new_tiles, new_loc])
+    
+    if (loc.j > 0):
+        new_loc = location(loc.i, loc.j-1)
+        new_tiles = deepcopy(tiles)
+        new_tiles[loc.i][loc.j], new_tiles[new_loc.i][new_loc.j] = new_tiles[new_loc.i][new_loc.j], new_tiles[loc.i][loc.j]
+        moves.append([new_tiles, new_loc])
+
+    if (loc.i < N-1):
+        new_loc = location(loc.i+1, loc.j)
+        new_tiles = deepcopy(tiles)
+        new_tiles[loc.i][loc.j], new_tiles[new_loc.i][new_loc.j] = new_tiles[new_loc.i][new_loc.j], new_tiles[loc.i][loc.j]
+        moves.append([new_tiles, new_loc])
+    
+    if (loc.j < N-1):
+        new_loc = location(loc.i, loc.j+1)
+        new_tiles = deepcopy(tiles)
+        new_tiles[loc.i][loc.j], new_tiles[new_loc.i][new_loc.j] = new_tiles[new_loc.i][new_loc.j], new_tiles[loc.i][loc.j]
+        moves.append([new_tiles, new_loc])
+
+    for i in range(len(moves)):
+        printTile(moves[i][0])
+        # print(moves[i][1])
+
+    return moves
+
+def heuristic(target, current):
+    h = 0
+
+    for i in range(N):
+        for j in range(N):
+            if current[i][j] != target[i][j]:
+                h += 1
+
+    return h
 
 def main():
     # validation_unit_testing()
     
-    tiles = [["1","2","3"],["4","X","6"],["5","7","8"]]
-    printTile(tiles)
-    print(check_X(tiles))
+    tilesA = [["1","2","X"],["4","5","6"],["3","7","8"]]
+    printTile(tilesA)
+    # loc = check_X(tiles)
+    # print(loc)
+
+    tilesB = [["1","5","7"],["4","2","8"],["3","X","6"]]
+    printTile(tilesB)
+
+    print(heuristic(tilesA, tilesB))
+    
+
 
 if __name__ == '__main__':
     main()
