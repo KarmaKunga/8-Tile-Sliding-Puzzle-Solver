f488034e51dc64c7cadc75d86bc225126926e371
KarmaKunga <karmakungab@gmail.com>
2024-11-05 10:20:33 +0700

    Penambahan Unit Testing dan Mulai kode DFS
    - Penambahan fungsi unit testing untuk heuristic, tile_equal, check_X
    - Mulai menulis untuk DFS

--- 8TSP-Solver.py
+++ 8TSP-Solver.py
@@ -3,7 +3,7 @@
 
 from copy import deepcopy
 
-N = 3  # Tiles / Side
+N = 2  # Tiles / Side
 
 class location:
     def __init__(self, i, j):
@@ -13,7 +13,14 @@ class location:
     def __str__(self):
         return f"{self.i}, {self.j}"
 
+class iter_DFS:
+    def __init__(self, tiles, loc, prev_tiles):
+        self.tiles = tiles
+        self.location = loc
+        self.previous_tiles = prev_tiles
+
 def printTile(tiles):
+    """Function to print tiles"""
     def printLine():
         print("+", end="")
         for i in range(N):
@@ -52,6 +59,8 @@ def tiles_not_valid(tiles):
     return 0
 
 def validation_unit_testing():
+    print("tiles_not_valid Test")
+    
     tiles = [["1","2","3"],["4","5","6"],["X","7","8"]]
     print(tiles_not_valid(tiles))
     
@@ -70,6 +79,13 @@ def check_X(tiles):
             if tiles[y][x] == "X":
                 return location(y, x)                
     return None
+
+def check_X_unit_testing():
+    print("chech_X Test")
+    tiles = [["1","2","X"],["4","5","6"],["3","7","8"]]
+    printTile(tiles)
+    loc = check_X(tiles)
+    print(loc)
 
 def possible_moves(tiles, loc):
     moves = []
@@ -114,18 +130,88 @@ def heuristic(target, current):
 
     return h
 
-def main():
-    # validation_unit_testing()
-    
+def heuristic_unit_testing():
+    print("heuristic Test")
     tilesA = [["1","2","X"],["4","5","6"],["3","7","8"]]
-    printTile(tilesA)
-    # loc = check_X(tiles)
-    # print(loc)
+    printTile(tilesA)   
 
     tilesB = [["1","5","7"],["4","2","8"],["3","X","6"]]
     printTile(tilesB)
 
     print(heuristic(tilesA, tilesB))
+
+def tile_equal(A, B):
+    for i in range(N):
+        for j in range(N):
+            if A[i][j] != B[i][j]:
+                return False
+            
+    return True
+
+def equal_unit_testing():
+    print("heuristic Test")
+
+    tilesA = [["1","2","X"],["4","5","6"],["3","7","8"]]
+    printTile(tilesA)
+    tilesB = [["1","5","7"],["4","2","8"],["3","X","6"]]
+    printTile(tilesB)
+
+    print(tile_equal(tilesA, tilesB))
+
+    printTile(tilesA)
+    tilesB = [["1","2","X"],["4","5","6"],["3","7","8"]]
+    printTile(tilesB)
+
+    print(tile_equal(tilesA, tilesB))
+
+def trace_back():
+    print("Trace back moves")
+
+def DFS_solver(target, start):
+    closed_list = []
+    open_list = []
+
+    open_list.append([start, check_X(start)])
+
+    while not open_list:
+        current_tile = open_list[0][0]
+
+        if tile_equal(current_tile, target):
+            trace_back()
+        else:
+            current_loc = open_list[0][1]
+            open_list = open_list[1:]
+
+            for i in possible_moves(current_tile, current_loc):
+                pass
+
+        
+    
+    
+
+
+
+def main():
+    # validation_unit_testing()
+    # check_X_unit_testing()
+    # heuristic_unit_testing()
+    # equal_unit_testing()
+    
+    ## 3x3
+    # tilesA = [["1","2","X"],["4","5","6"],["3","7","8"]]
+    # printTile(tilesA)
+
+    # tilesB = [["1","5","7"],["4","2","8"],["3","X","6"]]
+    # printTile(tilesB)
+
+    # 2x2
+    tilesA = [["1","X"],["3","2"]]
+    printTile(tilesA)
+
+    # tilesB = [["1","3"],["X","2"]]
+    # printTile(tilesB)
+
+    # print(type(possible_moves(tilesA, check_X(tilesA))[0][1]))
     
 
 
