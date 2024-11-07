3cc6044fe9beaf9b146ae8259ac39a4d0313bfb7
KarmaKunga <karmakungab@gmail.com>
2024-11-07 13:40:07 +0700

    Menyelesaikan kode
    - Membuat flowchart kode
    - Menyelesaikan BFS Solver (Sebelumnya salah nama: DFS)

--- 8TSP-Solver.py
+++ 8TSP-Solver.py
@@ -3,7 +3,7 @@
 
 from copy import deepcopy
 
-N = 2  # Tiles / Side
+N = 3  # Tiles / Side
 
 class location:
     def __init__(self, i, j):
@@ -13,7 +13,7 @@ class location:
     def __str__(self):
         return f"{self.i}, {self.j}"
 
-class iter_DFS:
+class BFS_node:
     def __init__(self, tiles, loc, prev_tiles):
         self.tiles = tiles
         self.location = loc
@@ -114,9 +114,9 @@ def possible_moves(tiles, loc):
         new_tiles[loc.i][loc.j], new_tiles[new_loc.i][new_loc.j] = new_tiles[new_loc.i][new_loc.j], new_tiles[loc.i][loc.j]
         moves.append([new_tiles, new_loc])
 
-    for i in range(len(moves)):
-        printTile(moves[i][0])
-        # print(moves[i][1])
+    # for i in range(len(moves)):
+    #     printTile(moves[i][0])
+    #     print(moves[i][1])
 
     return moves
 
@@ -164,27 +164,83 @@ def equal_unit_testing():
 
     print(tile_equal(tilesA, tilesB))
 
-def trace_back():
-    print("Trace back moves")
-
-def DFS_solver(target, start):
+def BFS_solver(target, start):
     closed_list = []
     open_list = []
 
-    open_list.append([start, check_X(start)])
+    def retrace(last_node):
 
-    while not open_list:
-        current_tile = open_list[0][0]
+        print("Trace back moves")
+        prev_tile = last_node.previous_tiles
 
-        if tile_equal(current_tile, target):
-            trace_back()
+        moves = [last_node.tiles]
+        
+        while (prev_tile != None):
+
+            for i in closed_list:
+                if tile_equal(prev_tile, i.tiles):
+                    prev_tile = i.previous_tiles
+                    moves.append(i.tiles)
+                    break
+        
+        for i in range(len(moves)-1, -1, -1):
+            printTile(moves[i])
+
+    open_list.append(BFS_node(start, check_X(start), None))
+
+    while 1:
+        
+        if open_list:
+
+            current_node = open_list[0]
+
+            print("current node")
+            printTile(current_node.tiles)
+
+            if tile_equal(current_node.tiles, target):
+                retrace(current_node)
+                break
+            else:
+                open_list = open_list[1:]
+
+                # print("nodes in open list")
+                # for i in open_list:
+                #     printTile(i.tiles)
+
+                print("Finding possible moves")
+                for neighbour in possible_moves(current_node.tiles, current_node.location):
+                    in_closed_list = False
+
+                    for j in closed_list:
+                        if (j.tiles == neighbour[0]):
+                            in_closed_list = True
+                            break
+
+                    if (not in_closed_list):
+                        in_open_list = False
+
+                        for j in open_list:
+                            if (j.tiles == neighbour[0]):
+                                in_open_list = True
+                                break
+
+                        if (not in_open_list):
+                            open_list.append(BFS_node(neighbour[0], neighbour[1], current_node.tiles))
+                
+                closed_list.append(current_node)
+
+                print("nodes in open list")
+                for i in open_list:
+                    printTile(i.tiles)
+                
+                print("nodes in closed list")
+                for i in closed_list:
+                    printTile(i.tiles)
+
+                # break
         else:
-            current_loc = open_list[0][1]
-            open_list = open_list[1:]
-
-            for i in possible_moves(current_tile, current_loc):
-                pass
-
+            print("Solution not found")
+            break
         
     
     
@@ -198,18 +254,36 @@ def main():
     # equal_unit_testing()
     
     ## 3x3
-    # tilesA = [["1","2","X"],["4","5","6"],["3","7","8"]]
-    # printTile(tilesA)
-
-    # tilesB = [["1","5","7"],["4","2","8"],["3","X","6"]]
-    # printTile(tilesB)
-
-    # 2x2
-    tilesA = [["1","X"],["3","2"]]
+    print("Target:")
+    tilesA = [["1","2","X"],["4","5","6"],["3","7","8"]]
     printTile(tilesA)
 
-    # tilesB = [["1","3"],["X","2"]]
+    # print("Start:")
+    # tilesB = [["2","4","6"],["1","7","5"],["3","X","6"]]
     # printTile(tilesB)
+
+    # print("Start:")
+    # tilesB = [["X","1","2"],["4","5","6"],["3","7","8"]]
+    # printTile(tilesB)
+
+    print("Start:")
+    tilesB = [["4","1","2"],["5","X","6"],["3","7","8"]]
+    printTile(tilesB)
+
+    # 2x2
+    # tilesA = [["1","X"],["3","2"]]
+    # printTile(tilesA)
+
+    # tilesB = [["1","X"],["3","2"]]
+    # printTile(tilesB)
+    # tilesB = [["1","2"],["X","3"]]
+    # printTile(tilesB)
+
+    # possible_moves(tilesA, location(0,1))
+    # for i in possible_moves(tilesA, location(0,1)):
+    #     printTile(i[0])
+
+    BFS_solver(tilesA, tilesB)
 
     # print(type(possible_moves(tilesA, check_X(tilesA))[0][1]))
     
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,3 @@
- [["1","3"],["X","2"]]
-    # printTile(tilesB)
+
+    tilesB = [["X","1","2"],["4","5","6"],["3","7","8"]]
+    printTile(tilesB)
